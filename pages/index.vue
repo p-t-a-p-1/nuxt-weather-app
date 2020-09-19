702a0602b318111f98174909781e4fe65f367d61
p-t-a-p-1 <kb240225+1@gmail.com>
2020-09-19 17:41:11 +0900

    [update] カードのhtmlタグ追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,52 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">nuxt-weather-app</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class="c-contents">
+    <div class="c-card">
+      <div class="c-card_main">
+        <div class="c-card_main_left">
+          <p class="c-card_main_temp">18°</p>
+          <p class="c-card_main_weather">SUNNY</p>
+        </div>
+        <div class="c-card_main_right">
+          <p class="c-card_main_area">Sapporo</p>
+        </div>
       </div>
+      <ul class="c-card_week">
+        <li class="c-card_date">
+          <p class="c-card_date_day">MON</p>
+          <font-awesome-icon icon="sun" class="c-card_date_icon" />
+          <p class="c-card_date_weather">SUNNY</p>
+          <p class="c-card_date_temp">High<span>18°</span></p>
+          <p class="c-card_date_temp">Low<span>18°</span></p>
+        </li>
+        <li class="c-card_date">
+          <p class="c-card_date_day">TUE</p>
+          <font-awesome-icon icon="sun" class="c-card_date_icon" />
+          <p class="c-card_date_weather">SUNNY</p>
+          <p class="c-card_date_temp">High<span>18°</span></p>
+          <p class="c-card_date_temp">Low<span>18°</span></p>
+        </li>
+        <li class="c-card_date">
+          <p class="c-card_date_day">WED</p>
+          <font-awesome-icon icon="sun" class="c-card_date_icon" />
+          <p class="c-card_date_weather">SUNNY</p>
+          <p class="c-card_date_temp">High<span>18°</span></p>
+          <p class="c-card_date_temp">Low<span>18°</span></p>
+        </li>
+        <li class="c-card_date">
+          <p class="c-card_date_day">THU</p>
+          <font-awesome-icon icon="sun" class="c-card_date_icon" />
+          <p class="c-card_date_weather">SUNNY</p>
+          <p class="c-card_date_temp">High<span>18°</span></p>
+          <p class="c-card_date_temp">Low<span>18°</span></p>
+        </li>
+        <li class="c-card_date">
+          <p class="c-card_date_day">FRI</p>
+          <font-awesome-icon icon="sun" class="c-card_date_icon" />
+          <p class="c-card_date_weather">SUNNY</p>
+          <p class="c-card_date_temp">High<span>18°</span></p>
+          <p class="c-card_date_temp">Low<span>18°</span></p>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -29,35 +55,13 @@
 export default {}
 </script>
 
-<style>
-.container {
+<style lang="scss">
+.c-contents {
   margin: 0 auto;
   min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
