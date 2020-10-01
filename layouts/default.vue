ac0e226b3a6c697041ec37a4ba2d774993f4c8d7
p-t-a-p-1 <kb240225+1@gmail.com>
2020-10-01 22:12:48 +0900

    [update] CSS設計見直し、修正など

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,20 @@
 <template>
   <div>
     <Header />
-    <main class="main">
+    <main class="c-main">
       <Nuxt />
     </main>
   </div>
 </template>
 
-<style></style>
+<style lang="scss">
+.c {
+  &-main {
+    margin: auto;
+    max-width: 1280px;
+  }
+}
+</style>
 
 <script>
 import Header from '@/components/Header.vue'
--- pages/city/_name.vue
+++ pages/city/_name.vue
@@ -1,31 +1,27 @@
 <template>
-  <div class="c-contents">
-    <div class="c-card">
-      <div class="c-card_main">
-        <div class="c-card_main_left">
-          <p class="c-card_main_temp">{{ nowData.main.temp }}°</p>
-          <p class="c-card_main_weather">
+  <div class="c-main_content">
+    <div class="c-main_content_card">
+      <div class="c-main_content_card_main">
+        <div class="-left">
+          <p class="-temp">{{ nowData.main.temp }}°</p>
+          <p class="-weather">
             {{ nowData.weather[0].description }}
           </p>
         </div>
-        <div class="c-card_main_right">
-          <p class="c-card_main_area">{{ nowData.name }}</p>
+        <div class="-right">
+          <p class="-area">{{ nowData.name }}</p>
         </div>
       </div>
-      <ul class="c-card_week">
-        <li
-          v-for="(day, index) in fiveDayData"
-          :key="index"
-          class="c-card_date"
-        >
-          <p class="c-card_date_day">
+      <ul class="c-main_content_card_week">
+        <li v-for="(day, index) in fiveDayData" :key="index" class="-date">
+          <p class="-day">
             {{ day.dt_txt }}
-            <span class="c-card_date_str">{{ day.weekDay }}</span>
+            <span class="-str">{{ day.weekDay }}</span>
           </p>
-          <font-awesome-icon :icon="day.icon" class="c-card_date_icon" />
-          <p class="c-card_date_weather">{{ day.weather[0].description }}</p>
-          <p class="c-card_date_temp">
-            気温<span class="c-card_date_number">{{ day.main.temp }}°</span>
+          <font-awesome-icon :icon="day.icon" class="-icon" />
+          <p class="-weather">{{ day.weather[0].description }}</p>
+          <p class="-temp">
+            気温<span class="-number">{{ day.main.temp }}°</span>
           </p>
         </li>
       </ul>
@@ -94,63 +90,66 @@ export default {
 </script>
 
 <style lang="scss">
-.c-contents {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.c-card {
-  width: 85%;
-}
-.c-card_main {
-  display: flex;
-  justify-content: space-around;
-  &_right {
-    display: flex;
-    align-items: center;
-  }
-  &_temp {
-    font-size: 5.5rem;
-  }
-  &_weather {
-    font-size: 2.4rem;
-  }
-  &_area {
-    font-size: 3.5rem;
-  }
-}
-.c-card_week {
-  margin-block-start: 40px;
-  display: flex;
-  justify-content: space-around;
-}
-.c-card_date {
-  font-size: 2rem;
-  &_day {
-    padding-block-end: 1rem;
-  }
-  &_str {
-    display: block;
-  }
-  &_icon {
-    font-size: 5rem;
-    padding-block-end: 1rem;
-  }
-  &_weather {
-    padding-block-end: 1rem;
-  }
-  &_temp {
-    padding-block-end: 1rem;
-    span {
-      font-size: 2.4rem;
+.c {
+  &-main {
+    &_content {
+      margin: 0 auto;
+      min-height: 100vh;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      text-align: center;
+      &_card {
+        width: 85%;
+        &_main {
+          display: flex;
+          justify-content: space-around;
+          .-right {
+            display: flex;
+            align-items: center;
+          }
+          .-temp {
+            font-size: 5.5rem;
+          }
+          .-weather {
+            font-size: 2.4rem;
+          }
+          .-area {
+            font-size: 3.5rem;
+          }
+        }
+        &_week {
+          margin-block-start: 40px;
+          display: flex;
+          justify-content: space-around;
+          .-date {
+            font-size: 2rem;
+          }
+          .-day {
+            padding-block-end: 1rem;
+          }
+          .-str {
+            display: block;
+          }
+          .-icon {
+            font-size: 5rem;
+            padding-block-end: 1rem;
+          }
+          .-weather {
+            padding-block-end: 1rem;
+          }
+          .-temp {
+            padding-block-end: 1rem;
+            span {
+              font-size: 2.4rem;
+            }
+          }
+          .-number {
+            margin-left: 10px;
+          }
+        }
+      }
     }
-  }
-  &_number {
-    margin-left: 10px;
   }
 }
 </style>
