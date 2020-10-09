7c781c1eee5eb0e89e86f3972535d20c37572df2
p-t-a-p-1 <kb240225+1@gmail.com>
2020-10-09 23:39:22 +0900

    [update] トップの検索画面追加

--- pages/index.vue
+++ pages/index.vue
@@ -4,5 +4,23 @@
     <ul>
       <li><nuxt-link :to="`city/sapporo/`">Sapporo</nuxt-link></li>
     </ul>
+    <input v-model="searchStr" type="text" name="name" />
+    <button type="button" @click="submit">検索</button>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchStr: '',
+    }
+  },
+  methods: {
+    // 地名検索
+    submit() {
+      this.$router.push(`/city/${this.searchStr}`)
+    },
+  },
+}
+</script>
